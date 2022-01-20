fe7d436ba4f217e3fca1522e4334f1c5aa591d8e
Noori Fathima <noorifathima99@gmail.com>
2022-01-20 12:53:23 +0530

    login and register css file is added
    Some changes are done in connect function related to db.

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,3 +1,7 @@
 const mongoose = require("mongoose");
 
-module.exports = mongoose.connect("mongodb+srv://siva:siva@cluster0.c6pmx.mongodb.net/buywow"); 
+module.exports = () => {
+  return mongoose.connect(
+    "mongodb+srv://siva:siva@cluster0.c6pmx.mongodb.net/buywow"
+  );
+};
